32a8dc98f3b1fa1ebd30100333604235c5053d54
tommyacn <tommys.worker@gmail.com>
2019-01-13 22:21:04 -0300

    initializing

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 echo "Here are our files";
-$path = ".";
+$path = "maps";
 $dh = opendir($path);
 $i=1;
 while (($file = readdir($dh)) !== false) {
-    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+    if($file != "." && $file != ".." && $file != "$path" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
         echo "<a href='$maps/$file'>$file</a><br /><br />";
         $i++;
     }
